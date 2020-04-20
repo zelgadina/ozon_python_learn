bc424e5e9aed895c8553d00748cea71100d71052
zelgadina <zelgadina@yandex.ru>
2020-04-20 21:23:29 +0300

    fix initial user input functions

--- hometask2_guess_number.py
+++ hometask2_guess_number.py
@@ -1,11 +1,9 @@
 from random import randint
 from getpass import getuser
 
-NORMAL = 12
-NIGHTMARE = 9
-HELL = 6
-
-lvls = {NORMAL, NIGHTMARE, HELL}
+lvls = {'1': 12,
+		'2': 9,
+		'3': 6}
 
 def hello():
 	username = getuser()
@@ -20,33 +18,33 @@ def hello():
 def lvl_description():
 	print("""
 	Выбери уровень сложности (введи номер):
-	[1] normal ({normal} попыток)
-	[2] nightmare ({nightmare} попыток)
-	[3] hell ({hell} попыток)
+	[1] normal ({lvl_1} попыток)
+	[2] nightmare ({lvl_2} попыток)
+	[3] hell ({lvl_3} попыток)
 
-	""".format(normal=NORMAL, nightmare=NIGHTMARE, hell=HELL))
+	""".format(lvl_1=lvls['1'], lvl_2=lvls['2'], lvl_3=lvls['3']))
 
 def lvl_select():
 	while True:
 		try:
 			lvl_description()
-			lvl = input()
-			if lvl == NORMAL:
-				return NORMAL
-			if lvl == NIGHTMARE:
-				return NIGHTMARE
-			if lvl == HELL:
-				return HELL
+			lvl = input().strip()
+			if lvl_validator(lvl) is True:
+				return lvl
 			else:
 				print("Некоректное значение, попытайся снова.\n")
 				continue
 		except KeyboardInterrupt:
 			exit()
-		except:
-			print("Что-то пошло не так, попытайся снова.\n")
+#		except:
+#			print("Что-то пошло не так, попытайся снова.\n")
+
 
 def lvl_validator(lvl_user_input) -> bool:
 	if lvl_user_input in lvls:
 		return True
 	return False
 
+
+hello()
+lvl_select()
