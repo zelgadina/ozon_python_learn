7f2105b5ba0b27808cd8166f3ed610856b5616b1
zelgadina <zelgadina@yandex.ru>
2020-04-23 23:38:51 +0300

    lvlup for hometask3: the results are displayed in 8 columns

--- hometask3_multiplication_table.py
+++ hometask3_multiplication_table.py
@@ -1,3 +1,6 @@
+# Вывод таблицы умножения в восемь столбцов вида "a * b = c"
+
 for i in range(2, 10):
-	for j in range(2, 10):
-		print(f"{i} * {j} = {i * j}")
+    for j in range(2, 10):
+        print((f"{j} * {i} = {j * i}").ljust(10),end=' '*4)
+    print()
